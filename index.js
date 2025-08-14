3fc0204bd1579a47f797f9b0a1294e93eb74a325
Jeyn-o <jenova.fode@gmail.com>
2025-08-14 15:22:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+console.log(`Token length: ${process.env.TOKEN.length}`);
+console.log(`Token (first 5 chars): ${process.env.TOKEN.slice(0,5)}`);
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
@@ -19,3 +23,4 @@ client.on('messageCreate', message => {
 });
 
 client.login(process.env.TOKEN);
+
